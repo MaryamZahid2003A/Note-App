0e202d8bba6a71c793d9426f88979f301043a1dd
AmnaKhan2003 <qadeemsb@gmail.com>
2025-05-11 17:52:10 +0500

    commit done

--- server/db/db.js
+++ server/db/db.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 
 const connectToMongoDB = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://AmnaKhan:Amna2003@cluster0.m6ev7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    );
+    await mongoose.connect("mongodb://mongo:27017/mydatabase");
     console.log("Connected to MongoDB");
   } catch (error) {
     console.log("Error connecting to MongoDB", error.message);
